0dc02f8593f7aa95d601468e292ed68fd8fa0b64
korn-sudo <76937659+korn-sudo@users.noreply.github.com>
2022-02-14 13:19:31 +0800

    Update simple_obfs_prepare.sh

--- prepare/simple_obfs_prepare.sh
+++ prepare/simple_obfs_prepare.sh
@@ -6,22 +6,22 @@ tls
 
 
 get_input_obfs_mode(){
-    generate_menu_logic "${OBFUSCATION_WRAPPER[*]}" "混淆模式" "1"
+    generate_menu_logic "${OBFUSCATION_WRAPPER[*]}" "Obfuscated Mode Encryption" "1"
     shadowsocklibev_obfs="${optionValue}"
 }
 
 get_input_obfs_domain(){
     while true
         do
-        _read "请为simple-obfs输入用于混淆的域名 (默认: cloudfront.com):"
+        _read "Please enter a domain name for obfuscation for simple-obfs (default: cloudfront.com):"
         domain="${inputInfo}"
         [ -z "$domain" ] && domain="cloudfront.com"
         if ! judge_is_domain "${domain}"; then
-            _echo -e "请输入一个格式正确的域名."
+            _echo -e "Please enter a well-formed domain name."
             continue
         fi
         if ! judge_is_valid_domain "${domain}"; then
-            _echo -e "无法解析到IP，请输入一个正确有效的域名."
+            _echo -e "Unable to resolve to IP, please enter a valid domain name."
             continue
         fi
         _echo -r "  obfs-host = ${domain}"
